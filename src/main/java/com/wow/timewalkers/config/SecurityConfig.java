ac069f1f5b8a8adf6a0c95838587108f2d1887e1
Zach Ferguson <zferguson@oakland.edu>
2026-06-14 11:47:36 -0400

    feat: setup local config

--- src/main/java/com/wow/timewalkers/config/SecurityConfig.java
+++ src/main/java/com/wow/timewalkers/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.wow.timewalkers.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Profile("!local")
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
